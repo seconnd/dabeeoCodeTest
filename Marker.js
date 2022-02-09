a1eaa6e639907bceff27dc16b902aa5581816a60
seconnd <96675650+seconnd@users.noreply.github.com>
2022-02-09 10:52:09 +0900

    Update Marker.js

--- Marker.js
+++ Marker.js
@@ -14,7 +14,7 @@ Marker.prototype.CreateMarker = function() {
 
     this.marker = [];
     let markerNum = Math.ceil(Math.random() * 30);
-    let markerPositionX, markerPositionZ;
+    let markerPositionX, markerPositionY;
 
     for (var i = 0; i < markerNum; i++) {
 
@@ -24,10 +24,10 @@ Marker.prototype.CreateMarker = function() {
 
         // create random position data
         markerPositionX = Math.round(((Math.random() * 8) - 4) * 1000) / 1000;
-        markerPositionZ = Math.round(((Math.random() * 8) - 4) * 1000) / 1000;
+        markerPositionY = Math.round(((Math.random() * 8) - 4) * 1000) / 1000;
 
         // assign position data and enable
-        this.marker[i].setPosition(markerPositionX, 0.1, markerPositionZ);
+        this.marker[i].setPosition(markerPositionX, markerPositionY, 0.1);
         this.marker[i].enabled = true;
     }
 
